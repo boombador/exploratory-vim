6677e9933c6403b7cfa4f733138e21c1bc5d1763
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2019-05-29 19:48:08 -0400

    Rename, reorder to match post code

--- rplugin/python3/time-keeper.py
+++ rplugin/python3/time-keeper.py
@@ -11,17 +11,12 @@ def get_now():
 class Tracker(object):
 
     def __init__(self, nvim):
-        """
-        should not do any significant processing on startup, defer to a helper
-        _setup function which runs at most once
-        """
         self.nvim = nvim
         self.initialized = False
 
-    def _setup(self):
+    def _initialize(self):
         if not self.initialized:
-            # computationally expensive startup logic goes here, should be
-            # called by functions that depend on it having been run
+            # computationally expensive startup logic goes here
             self.working = False
             self.root = '~/tracking'
             with open(os.path.expanduser('{}/toggles.txt'.format(self.root)), 'r') as f:
@@ -33,31 +28,27 @@ class Tracker(object):
                         self.working = True
             self.initialized = True
 
-    @pynvim.command('TrackerToggle', nargs='*', range='')
-    def command_toggle(self, args, range):
-        self._setup()
-        message = str.join(' ', args)
-        if message:
-            message = ' - {}'.format(message)
-        now = datetime.datetime.now()
-        action = 'stopped' if self.working else 'started'
-        log_line = '{} at {}{}\n'.format(action, now, message)
-        with open(os.path.expanduser('{}/toggles.txt'.format(self.root)), 'a') as f:
-            f.write(log_line)
-        self.nvim.out_write(log_line)
-        self.working = not self.working
-
-    # setting nargs to 1 combines multiple words into one string
     @pynvim.command('TrackerEvent', nargs='1', range='')
     def command_event(self, args, range):
-        self._setup()
+        self._initialize()
         message = args[0]
         log_line = '{} : {}\n'.format(datetime.datetime.now(), message)
         with open(os.path.expanduser('{}/events.txt'.format(self.root)), 'a') as f:
             f.write(log_line)
         self.nvim.out_write(log_line)
 
-    # setting nargs to 1 combines multiple words into one string
+    @pynvim.command('TrackerToggle', nargs='*', range='')
+    def command_toggle(self, args, range):
+        self._initialize()
+        message = str.join(' ', args)
+        now = datetime.datetime.now()
+        action = 'stopped' if self.working else 'started'
+        log_line = '{} at {} - {}\n'.format(action, now, message)
+        with open(os.path.expanduser('{}/toggles.txt'.format(self.root)), 'a') as f:
+            f.write(log_line)
+        self.nvim.out_write(log_line)
+        self.working = not self.working
+
     @pynvim.command('TrackerTodo', nargs='1', range='')
     def command_todo(self, args, range):
         self._setup()
