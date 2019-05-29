5d3a72fc8986d7b86b18b73413ecc8a874a625cf
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2019-05-28 23:35:27 -0400

    Some basic tracking functionality

--- rplugin/python3/time-keeper.py
+++ rplugin/python3/time-keeper.py
@@ -2,6 +2,10 @@ import datetime
 import os
 import pynvim
 
+def get_now():
+    # FIXME: timezones and all that
+    return datetime.datetime.now()
+
 
 @pynvim.plugin
 class TimeKeeper(object):
@@ -10,16 +14,35 @@ class TimeKeeper(object):
         self.nvim = nvim
         self.working = False
 
-    @pynvim.command('ToggleWorking', nargs='*', range='')
-    def command_toggle(self, args, range):
-        # FIXME: timezones and all that
-        now = datetime.datetime.now()
+    @pynvim.command('TrackWorkToggle', nargs='*', range='')
+    def command_work_toggle(self, args, range):
+        now = get_now()
         action = 'stopped' if self.working else 'started'
-        log_line = '{} at {}'.format(action, now)
-        with open(os.path.expanduser('~/timekeeping.txt'), 'a') as f:
-            f.write(log_line + '\n')
+        log_line = '{} at {}\n'.format(action, now)
+        with open(os.path.expanduser('~/timekeeping-worktoggle.txt'), 'a') as f:
+            f.write(log_line)
+        self.nvim.out_write(log_line)
         self.working = not self.working
 
+
+    # setting nargs to 1 combines multiple words into one string
+    @pynvim.command('TrackWorkEvent', nargs='1', range='')
+    def command_work_event(self, args, range):
+        message = args[0]
+        log_line = '{} : {}\n'.format(get_now(), message)
+        with open(os.path.expanduser('~/timekeeping-workevents.txt'), 'a') as f:
+            f.write(log_line)
+        self.nvim.out_write(log_line)
+
+
+    # setting nargs to 1 combines multiple words into one string
+    @pynvim.command('TrackTodo', nargs='1', range='')
+    def command_todo(self, args, range):
+        message = args[0]
+        log_line = '[ ] {} ({})\n'.format(message, get_now())
+        with open(os.path.expanduser('~/todo.txt'), 'a') as f:
+            f.write(log_line)
+        self.nvim.out_write(log_line)
 
 # overview: track work sessions. each work session has start and end datetime, optional start and end notes
 # save each transition in a log file for later perusal, update statusline
